9f6e8dc3dd290f3d5292787101c9c4f339d0226c
Uvais Ahmad <uvss21@gmail.com>
2023-01-29 18:18:23 +0530

    some changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-mongoose.set('strictQuery', true);console.log("MongoB URL ",process.env.MONGODB_URL)
+mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB_URL);
 const db = mongoose.connection;
 
