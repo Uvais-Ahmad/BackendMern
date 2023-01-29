a2e8d3a5536b7868ff8bbd93f79d728f3a8c1508
Uvais Ahmad <uvss21@gmail.com>
2023-01-29 12:51:36 +0530

    debug

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URL);
 const db = mongoose.connection;
 
 db.on("error" , console.error.bind(console , "Error while connecting with the database "));
