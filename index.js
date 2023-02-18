b3d18beeb6036704a915a4a7fddac1efe9dfdcdc
Uvais Ahmad <uvss21@gmail.com>
2023-02-18 19:14:31 +0530

    remove code for s3

--- index.js
+++ index.js
@@ -7,55 +7,9 @@ const dotenv = require('dotenv').config();
 const db = require('./config/mongoose')
 const cors = require('cors');
 
-const aws = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
-//config for aws        ::::This is ALL S3 setup using multer We not used .
-aws.config.update({
-    secretAccessKey:process.env.S3_SECRET_KEY,
-    accessKeyId:process.env.S3_ACCESS_KEY,
-    region:process.env.S3_REGION
-})
-const BUCKET = process.env.S3_BUCKET;
-//create s3 instance
-const s3 = new aws.S3();
-const upload = multer({
-    storage: multerS3({
-        s3:s3,
-        bucket:BUCKET,
-        acl:'public-read',   //acess control link
-        key: ( req , file , cb)=> {
-            console.log("File in upload ",file);
-            cb(null , file.originalname)
-        }
-    })
-})
-
-app.post('/upload',upload.single('file') , async ( req , res )=>{   // .single(feild name of receving file)
-    console.log(req.file)
-    res.send(`Successfully uploaded to s3 ${req.file.location} location`)
-})
-
-app.get('/list' ,async ( req , res )=>{
-    let response = await s3.listObjectsV2({Bucket:BUCKET}).promise();
-    //response contain lot of info so require fecth
-    let allkeys = await response.Contents.map(item =>item.Key);
-    res.send(allkeys);
-})
-
-app.get('/download/:filename', async ( req , res )=>{
-
-    const filename = req.params.filename;
-    let x = await s3.getObject({Bucket:BUCKET,Key:filename}).promise();
-    res.send(x.Body);
-})
-
-app.delete('/delete/:filename', async ( req , res )=>{
-
-    const filename = req.params.filename;
-    await s3.deleteObject({Bucket:BUCKET,Key:filename}).promise();
-    res.send("File deleted success");
-})
+// const aws = require('aws-sdk');
+// const multer = require('multer');
+// const multerS3 = require('multer-s3');
 
 app.use(cookieParser());
 app.use(express.urlencoded({extended : true}));
